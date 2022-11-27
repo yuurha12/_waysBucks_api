8a2696eefce13ee08cf5185e3ffca3d13be0196f
Kiki <kiki@pop-os.localdomain>
2022-11-27 10:49:59 +0700

    add condition for admin

--- routes/topping.go
+++ routes/topping.go
@@ -13,8 +13,8 @@ func ToppingRoutes(r *mux.Router) {
 	toppingRepository := repositories.RepositoryTopping(mysql.DB)
 	h := handlers.HandlerTopping(toppingRepository)
 
-	r.HandleFunc("/toppings", middleware.Auth(h.FindToppings)).Methods("GET")
-	r.HandleFunc("/topping/{id}", h.GetTopping).Methods("GET")
+	r.HandleFunc("/toppings", h.FindToppings).Methods("GET")
+	r.HandleFunc("/topping/{id}",middleware.Auth (h.GetTopping)).Methods("GET")
 	// Create "/topping" route using middleware Auth, middleware UploadFile, handler Createtopping, and method POST
 	r.HandleFunc("/topping", middleware.Auth(middleware.UploadFile(h.CreateTopping))).Methods("POST")
 	r.HandleFunc("/topping/{id}", h.DeleteTopping).Methods("DELETE")
