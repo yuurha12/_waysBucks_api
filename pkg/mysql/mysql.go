46cef400876574ee8a4572ba040fb2d693d1f5dd
Kiki <kiki@pop-os.localdomain>
2022-12-05 03:54:12 +0700

    test deploy2

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -2,7 +2,7 @@ package mysql
 
 import (
 	"fmt"
-	// "os"
+	"os"
 
 	"gorm.io/driver/mysql"
 	// "gorm.io/driver/postgres"
@@ -14,18 +14,18 @@ var err error
 
 func DatabaseInit() {
 
-	// var DB_HOST = os.Getenv("DB_HOST")
-	// var DB_USER = os.Getenv("DB_USER")
-	// var DB_PASSWORD = os.Getenv("DB_PASSWORD")
-	// var DB_NAME = os.Getenv("DB_NAME")
-	// var DB_PORT = os.Getenv("DB_PORT")
+	var DB_HOST = os.Getenv("DB_HOST")
+	var DB_USER = os.Getenv("DB_USER")
+	var DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	var DB_NAME = os.Getenv("DB_NAME")
+	var DB_PORT = os.Getenv("DB_PORT")
 
-	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
-	// DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	//todo if using mysql
-	dsn := "root:121295@tcp(localhost:3306)/waysbucks?charset=utf8mb4&parseTime=True&loc=Local"
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// dsn := "root:121295@tcp(localhost:3306)/waysbucks?charset=utf8mb4&parseTime=True&loc=Local"
+	// DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
