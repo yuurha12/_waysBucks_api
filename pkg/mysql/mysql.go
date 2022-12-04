88ff92730e0c7b10c91fc94c922f87b7ec864388
Kiki <kiki@pop-os.localdomain>
2022-12-05 03:58:34 +0700

    test deploy2

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -5,7 +5,6 @@ import (
 	"os"
 
 	"gorm.io/driver/mysql"
-	// "gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -20,12 +19,16 @@ func DatabaseInit() {
 	var DB_NAME = os.Getenv("DB_NAME")
 	var DB_PORT = os.Getenv("DB_PORT")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
+	// DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	//todo if using mysql
 	// dsn := "root:121295@tcp(localhost:3306)/waysbucks?charset=utf8mb4&parseTime=True&loc=Local"
 	// DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// ===== If using MySql =====
+	dsn := fmt.Sprintf("%s:@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
